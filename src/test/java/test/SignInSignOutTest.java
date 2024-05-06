ae2aeba6e7c9b370794c1f3e1fd5ba7665c90700
ashif0538 <ashif.alam@zopsmart.com>
2024-05-06 18:20:05 +0530

    refactored

--- src/test/java/test/SignInSignOutTest.java
+++ src/test/java/test/SignInSignOutTest.java
@@ -1,28 +1,28 @@
-//package test;
-//
-//import baseTest.MobileBaseTest;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//import pages.HomePage;
-//import pages.LeftHandNavigationPage;
-//import pages.SignInPage;
-//import utiles.MobileLoginUtility;
-//import utiles.MobileLogoutUtility;
-//
-//import java.util.Map;
-//
-//public class SignInSignOutTest extends MobileBaseTest {
-//
-//    @Test(description = "performing logIn and logOut test")
-//    public void signinSignoutTest(Map<String,String> data){
-//        new HomePage().clickOnAllowLocationAccess().clickOnAccountButton();
-//        MobileLoginUtility.login(data.get("UserName"),data.get("Password"));
-//        String loggedInUser = new HomePage().clickOnHamburgerIcon().getUserName("verfying userName");
-//        Assert.assertNotEquals(loggedInUser,"Sign In / Sign Up");
-//        new LeftHandNavigationPage().clickOnUserName();
-//        MobileLogoutUtility.logout();
-//        new HomePage().clickOnAccountButton();
-//        String signUpHeading = new SignInPage().getHeading("verfying Heading");
-//        Assert.assertEquals(signUpHeading,"Signup / Login");
-//    }
-//}
+package test;
+
+import baseTest.MobileBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LeftHandNavigationPage;
+import pages.SignInPage;
+import utiles.MobileLoginUtility;
+import utiles.MobileLogoutUtility;
+
+import java.util.Map;
+
+public class SignInSignOutTest extends MobileBaseTest {
+
+    @Test(description = "performing logIn and logOut test")
+    public void signinSignoutTest(Map<String,String> data){
+        new HomePage().clickOnAllowLocationAccess().clickOnAccountButton();
+        MobileLoginUtility.login(data.get("UserName"),data.get("Password"));
+        String loggedInUser = new HomePage().clickOnHamburgerIcon().getUserName("verfying userName");
+        Assert.assertNotEquals(loggedInUser,"Sign In / Sign Up");
+        new LeftHandNavigationPage().clickOnUserName();
+        MobileLogoutUtility.logout();
+        new HomePage().clickOnAccountButton();
+        String signUpHeading = new SignInPage().getHeading("verfying Heading");
+        Assert.assertEquals(signUpHeading,"Signup / Login");
+    }
+}
