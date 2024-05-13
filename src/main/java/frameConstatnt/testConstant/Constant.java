24c1e0d85c4eb14119a1860c2945a863138d26e4
Satyajeetkumar3 <satyajeet.kumar@zopsmart.com>
2024-05-13 17:30:47 +0530

    refactored

--- src/main/java/frameConstatnt/testConstant/Constant.java
+++ src/main/java/frameConstatnt/testConstant/Constant.java
@@ -11,7 +11,7 @@ public class Constant {
 
     // Search Product Test Constants
     public static final String CATEGORY_NAME = "FRUITS AND VEGETABLES";
-    public static final String ITEM_NAME = "Cut Mixed Fruit -350G";
+    public static final String ITEM_NAME = "Chilli Red - 100G";
     public static final String SEARCH_TEST_NAME_1 = "Category Search";
     public static final String SEARCH_TEST_NAME_2 = "Item Search";
 
