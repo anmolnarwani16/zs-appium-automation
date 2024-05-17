559b51fb59cd8b0d6b9b0906276321d5a21b618d
ashif0538 <ashif.alam@zopsmart.com>
2024-05-17 12:02:37 +0530

    refactored test

--- src/main/java/pages/MyAccountPage.java
+++ src/main/java/pages/MyAccountPage.java
@@ -5,14 +5,10 @@ import enums.WaitStrategy;
 import factories.MobileExplicitWaitFactories;
 import frameConstatnt.testConstant.Constant;
 import junit.framework.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import reports.MobileTestLog;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
 import java.util.Arrays;
 import java.util.List;
 
